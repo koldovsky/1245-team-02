ce0eb04cf7a1a23eca4a2401a7723ce328261dc5
serswik <59996285+serswik@users.noreply.github.com>
2024-08-31 16:01:11 +0000

    Our tutors carousel done

--- javascript/our-tutors.js
+++ javascript/our-tutors.js
@@ -6,52 +6,12 @@ const cardWidth = carousel.querySelector('.tutor-card').offsetWidth + parseInt(g
 let scrollAmount = 0;
 let maxScrollAmount = cardWidth * (carousel.children.length - 1);
 
-<<<<<<< HEAD
 function updateCarousel() {
   if (window.innerWidth < 768) {
     carousel.style.scrollSnapType = 'x mandatory';
   } else {
     carousel.style.scrollSnapType = 'none';
   }
-=======
-  function updateCarousel() {
-    const cardWidth = cards[0].offsetWidth;
-    const carouselWidth = carousel.offsetWidth;
-    const visibleCards = Math.floor(carouselWidth / cardWidth);
-
-    console.log('cardWidth:', cardWidth);
-    console.log('carouselWidth:', carouselWidth);
-    console.log('visibleCards:', visibleCards);
-    console.log('index:', index);
-
-    if (index < 0) index = 0;
-    if (index > cards.length - visibleCards) index = cards.length - visibleCards;
-
-    carousel.style.transform = `translateX(-${index * cardWidth}px)`;
-    prevButton.disabled = index === 0;
-    nextButton.disabled = index >= cards.length - visibleCards;
-  }
-  
-  prevButton.addEventListener('click', () => {
-    if (index > 0) {
-      index--;
-      updateCarousel();
-    }
-  });
-
-  nextButton.addEventListener('click', () => {
-    const cardWidth = cards[0].offsetWidth;
-    const carouselWidth = carousel.offsetWidth;
-    const visibleCards = Math.floor(carouselWidth / cardWidth);
-
-    if (index < cards.length - visibleCards) {
-      index++;
-      updateCarousel();
-    }
-  });
-
-  updateCarousel();
->>>>>>> b09d234b1c1be89c4b889c1b08f8bd73a0716613
 }
 
 function scrollToPosition(position) {
